4b4a49fa6e79f4790e1117c9d3151348677acc1d
shubham sharma <shubhamtech004@gmail.com>
2025-03-12 20:32:48 +0530

    project setup done also with the db connection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import 'dotenv/config';
 
 import {server} from './app.js';
+import connectDB from './Src/Db/dbConnection.js';
 
 // port of the project
 
@@ -11,7 +12,7 @@ const port = process.env.PORT || 5000;
 // server listen
 
 server.listen(port, ()=>{
-//    connectDB();
+   connectDB();
 
     console.log(`server is fired sucessfully on port number::) ${port}`);
 })
