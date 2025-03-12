854073af69cdb15badbd6709e5b31a8473cb5fe6
shubham sharma <shubhamtech004@gmail.com>
2025-03-13 00:29:35 +0530

    user model done

---
+++ Src/Controller/userController.js
@@ -0,0 +1,25 @@
+import { User } from "../Models/userModel";
+
+export const register = async(req,res)=>{
+     try {
+          const {name,email ,role} = req.body;
+          if(!name || !email || !role){
+             return res.status(400).json("All feild are required");
+          }
+        const checkEmail = await User.findOne({email});
+        if(checkEmail){
+            return res.status(400).json("Email Alredy exsit with other account");
+        }
+        const hashedPassword = await bcrypt.hash(password,10);
+        const user = await User.create({
+            name,
+            email,
+            role,
+            password:hashedPassword
+        })
+        return res.status(201).json(user,"registered sucessfully");
+     } catch (error) {
+        console.log(error);
+        
+     }
+}
